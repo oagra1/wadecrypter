225cb7b6be9548e02a72a38c32c703549ca6699a
oagra1 <gustavoagranomade@gmail.com>
2025-06-10 19:50:49 -0300

    Create logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,24 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
+  ),
+  defaultMeta: { 
+    service: 'whatsapp-media-decrypt',
+    version: process.env.npm_package_version || '1.0.0'
+  },
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    })
+  ]
+});
+
+module.exports = logger;
